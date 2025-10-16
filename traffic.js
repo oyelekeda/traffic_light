0f00888b1e96ffd55b805523632fe0a2f1e9a142
oyelekeda <oyelekedaniel@gmail.com>
2025-10-16 13:04:20 +0100

    first

---
+++ traffic.js
@@ -0,0 +1,11 @@
+function turn1() {
+  document.getElementById("light").setAttribute("src", "");
+}
+
+function turn2() {
+  document.getElementById("light").setAttribute("src", "images/off.jpg");
+}
+
+function turn3() {
+  document.getElementById("light").setAttribute("src", "images/off.jpg");
+}
